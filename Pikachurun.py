48354fbd8fa6b009106ab2d6d99858fade492731
Kartikay Bhasin <77197475+kartikay22@users.noreply.github.com>
2021-10-03 17:40:06 +0530

    Create Pikachurun.py

---
+++ Pikachurun.py
@@ -0,0 +1,2 @@
+import os
+os.system("git clone https://github.com/kartikay22/Pikachu-X /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
