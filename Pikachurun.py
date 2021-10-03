9622b3458e4d171f8f6069b0b22b1746a5ca4c71
Kartikay Bhasin <77197475+kartikay22@users.noreply.github.com>
2021-10-03 17:45:09 +0530

    Update Pikachurun.py

--- Pikachurun.py
+++ Pikachurun.py
@@ -1,2 +1,3 @@
+##os.system("git clone https://github.com/kartikay22/Pikachu-X /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
 import os
-os.system("git clone https://github.com/kartikay22/Pikachu-X /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
+os.system("git clone https://github.com/kartikay22/Pikachu-X && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
